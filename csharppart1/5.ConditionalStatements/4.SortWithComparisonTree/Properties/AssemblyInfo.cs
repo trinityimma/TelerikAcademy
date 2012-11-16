757a616496469e907b013fbacec56d6c2e9aa45c
Jason Zhekov <jasssonpet@gmail.com>
2012-11-16 20:51:17 +0200

    Renaming

--- csharppart1/5.ConditionalStatements/4.SortWithComparisonTree/Properties/AssemblyInfo.cs
+++ csharppart1/5.ConditionalStatements/4.SortWithComparisonTree/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("4.SortWIthComparisonTree")]
+[assembly: AssemblyTitle("4.SortWithComparisonTree")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("4.SortWIthComparisonTree")]
+[assembly: AssemblyProduct("4.SortWithComparisonTree")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00627b69-294e-4033-b0e3-3140e1b85594")]
+[assembly: Guid("dbc81538-efba-40f3-a4ec-c16f89bd63ee")]
 
 // Version information for an assembly consists of the following four values:
 //
