00459f58f86a8c3d329f1cd591f8e7350b1a3de3
Jason Zhekov <jasssonpet@gmail.com>
2012-11-17 20:03:53 +0200

    Update FallingRocks

--- csharppart1/4.ConsoleInputOutput/10.LeibnizSeries/Program.cs
+++ csharppart1/4.ConsoleInputOutput/10.LeibnizSeries/Program.cs
@@ -6,9 +6,7 @@ class Program
     {
         double sum = 0;
         for (int i = 1, j = 1; i < 1001; i++, j *= -1)
-        {
             sum += 1.0 / i * j;
-        }
         Console.WriteLine(sum);
     }
 }
--- csharppart1/4.ConsoleInputOutput/11.FallingRocks/Program.cs
+++ csharppart1/4.ConsoleInputOutput/11.FallingRocks/Program.cs
@@ -1,92 +1,144 @@
-﻿using System;
+﻿﻿using System;
 using System.Threading;
 
 // TODO: Learn OOP in C#
 class Program
 {
-    static int playerPosition = 0;
-    static bool collision = false;
-    static int result = 0;
+    // Variables
+    static int[,] rocks = new int[15, 5]; // [N rocks][x, y, width, char, color index]
+    static double result = 0;
+    static int playerPositionX;
+    static int playerPositionY;
+    static bool collision;
+    static double fps;
 
-    static string player = "(0)";
-    static int[,] rocks = new int[15, 4]; // [N rocks][x, y, width, char]
+    // Constants
+    static string player = "(O)";
     static char[] rockCharacters = { '^', '@', '*', '&', '+', '%', '$', '#', '!', '.', ';' };
+    static String[] rockColors = { "Green", "Yellow", "Red" };
     static int rockMaxWidth = 3;
-    static int rockBottomPaddingAtStart = 4;
-    static int fps = 20;
+    static int rockBottomPaddingAtStart = rockMaxWidth + player.Length;
+    static int minFps = 15, maxFps = 40;
     static Random randomGenerator = new Random();
 
-    static void PrintAtPosition(int x, int y, string s)
+    static void PrintAtPosition(int x, int y, string s, string color)
     {
+        Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color);
         Console.SetCursorPosition(x, y);
         Console.Write(s);
     }
 
-    static void GetRock(int i)
+    static void CalculateFPS()
     {
-        rocks[i, 2] = 1 + randomGenerator.Next(rockMaxWidth); // width
-        rocks[i, 3] = randomGenerator.Next(rockCharacters.Length); // char
-        rocks[i, 0] = randomGenerator.Next(Console.WindowWidth - rocks[i, 2] + 1); // x
-        rocks[i, 1] = -1 + randomGenerator.Next(Console.WindowHeight - rockBottomPaddingAtStart); // y
+        fps += collision ? -15 : 0.5;
+        fps = Math.Min(Math.Max(fps, minFps), maxFps);
     }
 
-    static bool CheckCollision(int x, int y, int l)
+    static void CalculateResult()
     {
-        return y == Console.WindowHeight - 1 && x + l > playerPosition && playerPosition + player.Length > x;
+        result += collision ? -5 : .1;
+        result = Math.Max(result, 0);
     }
 
-    static void SetInitialPositions()
+    static void CheckCollision()
     {
-        playerPosition = (Console.WindowWidth - player.Length) / 2;
-        for (int i = 0; i < rocks.GetLength(0); i++) GetRock(i);
-    }
-
-    static void MovePlayer(ConsoleKeyInfo keyInfo)
-    {
-        if (keyInfo.Key == ConsoleKey.LeftArrow && playerPosition > 0) playerPosition--;
-        if (keyInfo.Key == ConsoleKey.RightArrow && playerPosition + player.Length < Console.WindowWidth) playerPosition++;
-    }
-
-    static void DrawPlayer()
-    {
-        PrintAtPosition(playerPosition, Console.WindowHeight - 1, player);
-    }
-
-    static void DrawEnviroment()
-    {
-        collision = false;
         for (int i = 0; i < rocks.GetLength(0); i++)
-        {
-            rocks[i, 1]++; // y++
-            collision = collision || CheckCollision(rocks[i, 0], rocks[i, 1], rocks[i, 2]);
-            PrintAtPosition(rocks[i, 0], rocks[i, 1], new String(rockCharacters[rocks[i, 3]], rocks[i, 2]));
-
-            // Fallen below viewport -> add new one on top
-            if (rocks[i, 1] == Console.WindowHeight - 1)
-            {
-                GetRock(i);
-                rocks[i, 1] = -1;
-            }
-        }
+            collision = collision || rocks[i, 1] == playerPositionY && rocks[i, 0] + rocks[i, 2] > playerPositionX && playerPositionX + player.Length > rocks[i, 0];
     }
 
     static void DrawResult()
     {
-        result += collision ? -50 : 1;
-        PrintAtPosition(0, 0, Convert.ToString(result / 10));
+        PrintAtPosition(0, 0, "Speed: " + (int)(fps - minFps) + " Result: " + (int)result, "Gray");
     }
 
-    static void Main(string[] args)
+    // ROCKS
+    static void GetRock(int i)
     {
-        SetInitialPositions();
+        rocks[i, 2] = 1 + randomGenerator.Next(rockMaxWidth); // width
+        rocks[i, 0] = randomGenerator.Next(Console.WindowWidth - rocks[i, 2] + 1); // x
+        rocks[i, 1] = -1 + randomGenerator.Next(Console.WindowHeight - rockBottomPaddingAtStart); // y
+        rocks[i, 3] = randomGenerator.Next(rockCharacters.Length); // char
+        rocks[i, 4] = randomGenerator.Next(rockColors.Length); // color
+    }
+
+    static void MoveRockTop(int i)
+    {
+        GetRock(i);
+        rocks[i, 1] = 0;
+    }
+
+    static void MoveRockDown(int i)
+    {
+        rocks[i, 1]++;
+    }
+
+    static void DrawRock(int i)
+    {
+        PrintAtPosition(rocks[i, 0], rocks[i, 1], new String(rockCharacters[rocks[i, 3]], rocks[i, 2]), rockColors[rocks[i, 4]]);
+    }
+
+    static bool IsRockVisible(int i)
+    {
+        return rocks[i, 1] < Console.WindowHeight;
+    }
+
+    static void InitRocks()
+    {
+        for (int i = 0; i < rocks.GetLength(0); i++) GetRock(i);
+    }
+
+    static void MoveRocks()
+    {
+        for (int i = 0; i < rocks.GetLength(0); i++)
+        {
+            MoveRockDown(i);
+            if (!IsRockVisible(i)) MoveRockTop(i);
+        }
+    }
+
+    static void DrawRocks()
+    {
+        for (int i = 0; i < rocks.GetLength(0); i++) DrawRock(i);
+    }
+
+    // PLAYER
+    static void CenterPlayer()
+    {
+        playerPositionX = (Console.WindowWidth - player.Length) / 2;
+        playerPositionY = Console.WindowHeight - 1;
+    }
+
+    static void DrawPlayer()
+    {
+        PrintAtPosition(playerPositionX, playerPositionY, player, "White");
+    }
+
+    static void MovePlayer(ConsoleKeyInfo keyInfo)
+    {
+        if (keyInfo.Key == ConsoleKey.LeftArrow && playerPositionX > 0) playerPositionX--;
+        if (keyInfo.Key == ConsoleKey.RightArrow && playerPositionX + player.Length < Console.WindowWidth) playerPositionX++;
+
+        if (keyInfo.Key == ConsoleKey.UpArrow && playerPositionY > 0) playerPositionY--;
+        if (keyInfo.Key == ConsoleKey.DownArrow && playerPositionY - 1 < Console.WindowWidth) playerPositionY++;
+    }
+
+    static void Main()
+    {
+        CenterPlayer();
+        InitRocks();
         while (true)
         {
+            collision = false;
             if (Console.KeyAvailable) MovePlayer(Console.ReadKey());
+            MoveRocks();
+            CheckCollision();
+            CalculateResult();
+            CalculateFPS();
             Console.Clear();
             DrawPlayer();
-            DrawEnviroment();
+            DrawRocks();
             DrawResult();
-            Thread.Sleep(1000 / fps);
+            Thread.Sleep(1000 / (int)fps);
         }
     }
 }
