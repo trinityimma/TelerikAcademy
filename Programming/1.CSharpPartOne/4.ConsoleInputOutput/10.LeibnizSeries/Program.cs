edc22729aef57d85e6e02ff3f564542d87cf088b
Jason Zhekov <jasssonpet@gmail.com>
2012-11-28 22:31:34 +0200

    Better sum of series

--- Programming/1.CSharpPartOne/4.ConsoleInputOutput/10.LeibnizSeries/Program.cs
+++ Programming/1.CSharpPartOne/4.ConsoleInputOutput/10.LeibnizSeries/Program.cs
@@ -4,9 +4,7 @@ class Program
 {
     static void Main()
     {
-        double sum = 0;
-        for (int i = 1, j = 1; i < 1001; i++, j *= -1)
-            sum += 1.0 / i * j;
-        Console.WriteLine(sum);
+        double sum = 1;
+        Console.WriteLine("{0:0.000} ", 2 - Math.Log(2));
     }
 }
