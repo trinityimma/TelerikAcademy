d6ea108352c2f2b96cc84866c5263d3929b22b41
Jason Zhekov <jasssonpet@gmail.com>
2013-01-14 22:10:28 +0200

    Update Programming/1.CSharpPartOne/9.Exam/1.NextDate/Program.cs

--- Programming/1.CSharpPartOne/9.Exam/1.NextDate/Program.cs
+++ Programming/1.CSharpPartOne/9.Exam/1.NextDate/Program.cs
@@ -9,7 +9,8 @@ class Program
         if (m == 2)
             if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) return 29;
             else return 28;
-        else return monthsLength[m - 1];
+
+        return monthsLength[m - 1];
     }
 
     static void Main()
