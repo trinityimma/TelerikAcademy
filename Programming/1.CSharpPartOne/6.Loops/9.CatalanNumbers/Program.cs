c12c0ca3110cc60bfaef1f8aa22ee5748d22ded9
Jason Zhekov <jasssonpet@gmail.com>
2013-01-14 22:14:59 +0200

    Update Programming/1.CSharpPartOne/6.Loops/9.CatalanNumbers/Program.cs

--- Programming/1.CSharpPartOne/6.Loops/9.CatalanNumbers/Program.cs
+++ Programming/1.CSharpPartOne/6.Loops/9.CatalanNumbers/Program.cs
@@ -6,7 +6,9 @@ class Program
     {
         int n = 10; // 0, 1, 2 ...
         double number = 1;
+
         for (int i = 2; i <= n; i++) number *= (4 * i - 2) / (1.0 + i);
+
         Console.WriteLine(number);
     }
 }
