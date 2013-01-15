ed1f97e0ca1134dfc85bf707837935f4f5a445c5
Jason Zhekov <jasssonpet@gmail.com>
2013-01-15 16:24:33 +0200

    Update Programming/2.CSharpPartTwo/1.Arrays/13.MergeSort/Program.cs

--- Programming/2.CSharpPartTwo/1.Arrays/13.MergeSort/Program.cs
+++ Programming/2.CSharpPartTwo/1.Arrays/13.MergeSort/Program.cs
@@ -11,7 +11,7 @@ class Program
             if (arr[i] < arr[j]) temp[k++] = arr[i++];
             else temp[k++] = arr[j++];
 
-        while (i <= m) temp[k++] = arr[i++]; // Copy items from first array
+        while (i <= m) temp[k++] = arr[i++]; // Copy items left from first array
         while (j <= r) temp[k++] = arr[j++]; // Or from second array
 
         for (i = l; i <= r; i++) arr[i] = temp[i]; // Save to the original array
@@ -21,12 +21,12 @@ class Program
     {
         if (l >= r) return;
 
-        int m = l + (r - l) / 2;
+        int m = l + (r - l) / 2; // Split in two
 
         MergeSort(arr, temp, l, m);
         MergeSort(arr, temp, m + 1, r);
 
-        Merge(arr, temp, l, m, r);
+        Merge(arr, temp, l, m, r); // And merge the sorted halves
     }
 
     static void Main()
