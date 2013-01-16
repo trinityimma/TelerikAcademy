13994842a72c4e833c22f7a44519c5f5a1d6ff9d
Jason Zhekov <jasssonpet@gmail.com>
2013-01-16 03:56:02 +0200

    Small fixes

--- Programming/2.CSharpPartTwo/4.NumeralSystems/1.Base10ToBase2/Program.cs
+++ Programming/2.CSharpPartTwo/4.NumeralSystems/1.Base10ToBase2/Program.cs
@@ -11,8 +11,14 @@ class Program
         return b;
     }
 
+    static string RecursionBase10ToBase2(int d, string b = "")
+    {
+        return d == 0 ? b : RecursionBase10ToBase2(d / 2, d % 2 + b);
+    }
+
     static void Main()
     {
         Console.WriteLine(Base10ToBase2(253));
+        Console.WriteLine(RecursionBase10ToBase2(253));
     }
 }
