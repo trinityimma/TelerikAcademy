85702c07fb9af3ca2c164078dd068ac4327514d4
Jason Zhekov <jasssonpet@gmail.com>
2013-01-16 20:49:37 +0200

    Add tokenizer

--- Programming/2.CSharpPartTwo/5.UsingClassesAndObjects/7.ParseExpression/Program.cs
+++ Programming/2.CSharpPartTwo/5.UsingClassesAndObjects/7.ParseExpression/Program.cs
@@ -1,37 +1,76 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    // Evaluate a postfix expression
-    static double EvaluateExpression(string expression)
+    // Read a string char by char and split it in tokens: "(1 + 2) * 3" -> "(", "1", "+", "2", ")", "*", "3"
+    static List<string> Tokenize(string s)
     {
-        double[] st = new double[64]; // Stack
-        int p = 0; // Stack pointer
+        List<string> tokens = new List<string>();
 
-        foreach (string token in expression.Split(' '))
-            if      (token == "+"   ) st[--p - 1] += st[p];
-            else if (token == "-"   ) st[--p - 1] -= st[p];
-            else if (token == "*"   ) st[--p - 1] *= st[p];
-            else if (token == "/"   ) st[--p - 1] /= st[p];
-            else if (token == "pow" ) st[--p - 1]  = Math.Pow  (st[p - 1], st[p]);
-            else if (token == "sqrt") st[  p - 1]  = Math.Sqrt (st[p - 1]);
-            else if (token == "ln"  ) st[  p - 1]  = Math.Log  (st[p - 1], Math.E);
-            else st[p++] = double.Parse(token);
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ') continue; // Skip white space
 
-        return st[0];
+            string token = String.Empty;
+
+            if (IsChar(s[i])) // String
+                for (; i < s.Length && IsChar(s[i]) || i-- == int.MaxValue; i++)
+                    token += s[i];
+
+            else if (IsDigit(s[i]) || (s[i] == '-' && IsDigit(s[i + 1]))) // Number
+                for (; i < s.Length && (IsDigit(s[i]) || s[i] == '.' || s[i] == '-') || i-- == int.MaxValue; i++)
+                    token += s[i];
+
+            else token += s[i]; // Operator
+
+            tokens.Add(token);
+        }
+
+        return tokens;
     }
 
-    // TODO: Parse an infix expression to postfix
-    static double ParseExpression(string infix)
+    // Parse an infix expression to postfix: "(", "1", "+", "2", ")", "*", "3" -> "1 2 + 3 *"
+    static string ParseExpression(List<string> infix)
     {
-        string postfix = "3 5.3 + 2.7 * 22 ln 2.2 -1.7 pow / -";
+        string postfix;
 
-        return EvaluateExpression(postfix);
+        // TODO: 
+
+        return "3 5.3 + 2.7 * 22 ln 2.2 -1.7 pow / -";
     }
 
+    // Evaluate a postfix expression - "1 2 + 3 *" -> 9
+    static double EvaluateExpression(string output)
+    {
+        var stack = new Stack<double>();
+
+        foreach (string token in output.Split(' '))
+            if (token == "+") stack.Push(stack.Pop() + stack.Pop());
+            else if (token == "-") stack.Push(-stack.Pop() + stack.Pop());
+            else if (token == "*") stack.Push(stack.Pop() * stack.Pop());
+            else if (token == "/") stack.Push(1 / stack.Pop() * stack.Pop());
+            else if (token == "ln") stack.Push(Math.Log(stack.Pop(), Math.E));
+            else if (token == "sqrt") stack.Push(Math.Sqrt(stack.Pop()));
+            else if (token == "pow") stack.Push(Math.Pow(y: stack.Pop(), x: stack.Pop())); // x ^ y
+            else stack.Push(double.Parse(token));
+
+        return stack.Pop();
+    }
 
     static void Main()
     {
-        Console.WriteLine(ParseExpression("(3 + 5.3) * 2.7 - ln(22) / pow(2.2, -1.7)"));
+        Console.WriteLine(EvaluateExpression(ParseExpression(Tokenize("(3 + 5.3) * 2.7 - ln(22) / pow(2.2, -1.7)"))));
+    }
+
+    // Helper methods
+    static bool IsChar(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+
+    static bool IsDigit(char c)
+    {
+        return c >= '0' && c <= '9';
     }
 }
