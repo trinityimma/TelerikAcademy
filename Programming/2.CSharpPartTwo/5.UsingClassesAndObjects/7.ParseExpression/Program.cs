0ca0851a9d6a9d210c40b11f9d59d81c2b77a7b2
Jason Zhekov <jasssonpet@gmail.com>
2013-01-19 01:46:24 +0200

    Update Programming/2.CSharpPartTwo/5.UsingClassesAndObjects/7.ParseExpression/Program.cs

--- Programming/2.CSharpPartTwo/5.UsingClassesAndObjects/7.ParseExpression/Program.cs
+++ Programming/2.CSharpPartTwo/5.UsingClassesAndObjects/7.ParseExpression/Program.cs
@@ -7,7 +7,7 @@ class Program
         { "pow", 4 }, { "sqrt", 4 }, { "ln", 4 }, { "*", 3 }, { "/", 3 }, { "+", 2 }, { "-", 2 }, { "(", 0 }, { ")", 999 }
     };
 
-    // Tokenize a string:  "(1 + 2) * 3" -> "(", "1", "+", "2", ")", "*", "3"
+    // Tokenize a string: "(1 + 2) * 3" -> "(", "1", "+", "2", ")", "*", "3"
     static List<Tuple<string, string>> Tokenize(string s)
     {
         var tokens = new List<Tuple<string, string>>(); // Value, type
