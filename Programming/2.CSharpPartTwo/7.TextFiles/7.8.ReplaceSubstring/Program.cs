c5d38c8e3c2b4f299e7d0e41a226a085d6dbe612
Jason Zhekov <jasssonpet@gmail.com>
2013-01-23 19:51:00 +0200

    Update Programming/2.CSharpPartTwo/7.TextFiles/7.8.ReplaceSubstring/Program.cs

--- Programming/2.CSharpPartTwo/7.TextFiles/7.8.ReplaceSubstring/Program.cs
+++ Programming/2.CSharpPartTwo/7.TextFiles/7.8.ReplaceSubstring/Program.cs
@@ -9,7 +9,7 @@ class Program
         using (StreamReader input = new StreamReader("../../input.txt"))
         using (StreamWriter output = new StreamWriter("../../output.txt"))
             for (string line; (line = input.ReadLine()) != null; )
-            //  output.WriteLine(line.Replace("start", "finish"));                         // Exercise 7
+            //  output.WriteLine(line.Replace("start", "finish"));             // Exercise 7
                 output.WriteLine(Regex.Replace(line, @"\bstart\b", "finish")); // Exercise 8
     }
 }
