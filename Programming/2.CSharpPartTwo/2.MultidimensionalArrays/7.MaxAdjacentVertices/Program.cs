35349278c06dd581ca3cf2c9494095e1842b99ef
Jason Zhekov <jasssonpet@gmail.com>
2013-01-23 17:01:59 +0200

    Update Programming/2.CSharpPartTwo/2.MultidimensionalArrays/7.MaxAdjacentVertices/Program.cs

--- Programming/2.CSharpPartTwo/2.MultidimensionalArrays/7.MaxAdjacentVertices/Program.cs
+++ Programming/2.CSharpPartTwo/2.MultidimensionalArrays/7.MaxAdjacentVertices/Program.cs
@@ -42,9 +42,13 @@ class Program
         int[,] matrix = { { 1, 3, 2, 2, 2, 4 }, { 3, 3, 3, 2, 4, 4 }, { 4, 3, 1, 2, 3, 3 }, { 4, 3, 1, 3, 3, 1 }, { 4, 3, 3, 3, 1, 1 } };
 
         int maxSum = 0;
+        
+        // For each cell
         for (int i = 0; i < matrix.GetLength(0); i++)
+        {
             for (int j = 0; j < matrix.GetLength(1); j++)
-                if (matrix[i, j] != 0)
+            {
+                if (matrix[i, j] != 0) // If not visited
                 {
                     currentSum = 0;
                     DFS(matrix, i, j);
@@ -54,6 +58,8 @@ class Program
                     PrintMatrix(matrix);
                     Console.WriteLine(currentSum + "\n");
                 }
+            }
+        }
 
         Console.WriteLine(maxSum);
     }
