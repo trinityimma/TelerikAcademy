7d9352f70e4eb93aac6aa259d45bf1e063654000
Jason Zhekov <jasssonpet@gmail.com>
2013-01-24 07:03:32 +0200

    Small fixes

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/13.ReverseWords/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/13.ReverseWords/Program.cs
@@ -9,17 +9,14 @@ class Program
         string str = "C#   is not C++, not PHP and not Delphi!  Test 1, 2,3.";
 
         string regex = @"\s+|,\s*|\.\s*|!\s*";
-        List<string> words = new List<string>();      // Stack
-        List<string> separators = new List<string>(); // Queue
+
+        Stack<string> words = new Stack<string>();
 
         foreach (string word in Regex.Split(str, regex))
-            words.Add(word);
+            if (!String.IsNullOrEmpty(word)) words.Push(word);
 
         foreach (Match separator in Regex.Matches(str, regex))
-            separators.Add(separator.Value);
-
-        for (int i = 0; i < separators.Count; i++)
-            Console.Write(words[words.Count - 2 - i] + separators[i]); // Without the last empty word
+            Console.Write(words.Pop() + separator);
 
         Console.WriteLine();
     }
