b73ce799287e0b2e50fb26a8e308e131c9860606
Jason Zhekov <jasssonpet@gmail.com>
2013-01-26 06:04:32 +0200

    Update Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/25.ExtractHTML/Program.cs

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/25.ExtractHTML/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/25.ExtractHTML/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        string str = @"<html>  <head><title>News</title></head><body><p><a href=""http://academy.telerik.com"">TelerikAcademy</a>aims to provide free real-world practicaltraining for young people who want to turn into skillful .NET software engineers.</p></body></html>";
+        string str = @"<html><head><title>News</title></head><body><p><a href=""http://academy.telerik.com"">TelerikAcademy</a>aims to provide free real-world practicaltraining for young people who want to turn into skillful .NET software engineers.</p></body></html>";
 
         foreach (Match text in Regex.Matches(str, ">(.*?)<"))
             if (!String.IsNullOrWhiteSpace(text.Groups[1].Value))
