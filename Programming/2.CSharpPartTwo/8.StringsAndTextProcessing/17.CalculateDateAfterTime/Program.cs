cad6f47d4bb80a24af44f556ce2917d74d7c1d6f
Jason Zhekov <jasssonpet@gmail.com>
2013-01-27 04:34:22 +0200

    Small fixes

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/17.CalculateDateAfterTime/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/17.CalculateDateAfterTime/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         string str = "24.01.2013 23:00:00";
 
-        DateTime date = DateTime.Parse(str).AddHours(6).AddMinutes(30);
+        DateTime date = DateTime.Parse(str).AddHours(6.5);
 
         Console.WriteLine("{0} {1}", date.ToString("dddd", new CultureInfo("bg-BG")), date);
     }
