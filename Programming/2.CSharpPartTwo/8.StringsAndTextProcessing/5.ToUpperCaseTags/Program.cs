03561d11109b52228fe19dfd2fc87a9664617946
Jason Zhekov <jasssonpet@gmail.com>
2013-01-30 00:58:31 +0200

    Small fixes

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/5.ToUpperCaseTags/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/5.ToUpperCaseTags/Program.cs
@@ -7,6 +7,6 @@ class Program
     {
         string str = "We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.";
 
-        Console.WriteLine(Regex.Replace(str, "<upcase>(.*?)</upcase>", matches => matches.Groups[1].Value.ToUpper()));
+        Console.WriteLine(Regex.Replace(str, "<upcase>(.*?)</upcase>", m => m.Groups[1].Value.ToUpper()));
     }
 }
