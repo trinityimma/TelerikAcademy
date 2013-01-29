c07cd276c7317f79d651e06fefb3b697b25380a1
Jason Zhekov <jasssonpet@gmail.com>
2013-01-29 06:34:09 +0200

    Update Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/18.ExtractEmails/Program.cs

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/18.ExtractEmails/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/18.ExtractEmails/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main()
     {
-        string str = "Static void Main() jasssonpet@gmail.com. using System,jason@abv.bg return";
+        string str = "Static void Main() nakov@telerik.com. using System,nakov@gmail.com return";
 
         foreach (var item in Regex.Matches(str, @"\w+@\w+\.\w+"))
             Console.WriteLine(item);
