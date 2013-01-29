39614a1188b65e9b36207852149080b5e0f02b0c
Jason Zhekov <jasssonpet@gmail.com>
2013-01-29 03:15:08 +0200

    Update Programming/1.CSharpPartOne/6.Loops/11.PlayingCardNames/Program.cs

--- Programming/1.CSharpPartOne/6.Loops/11.PlayingCardNames/Program.cs
+++ Programming/1.CSharpPartOne/6.Loops/11.PlayingCardNames/Program.cs
@@ -7,6 +7,7 @@ class Program
         string[] names = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
         string[] suits = { "spades", "diamonds", "hearts", "clubs" };
 
-        for (int i = 0; i < 52; i++) Console.WriteLine(names[i % 13] + " of " + suits[i % 4]);
+        for (int i = 0; i < 52; i++)
+            Console.WriteLine(names[i % 13] + " of " + suits[i % 4]);
     }
 }
