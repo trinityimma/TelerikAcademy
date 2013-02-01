40d371779c07a13ce94017e3c691f710f2873c08
Jason Zhekov <jasssonpet@gmail.com>
2013-02-01 04:24:04 +0200

    Small fixes

--- Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/19.ExtractDates/Program.cs
+++ Programming/2.CSharpPartTwo/8.StringsAndTextProcessing/19.ExtractDates/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -9,7 +9,7 @@ class Program
         string str = "Static void Main() 12.10.2012, 50.13.2012";
 
         DateTime date;
-        foreach (Match item in Regex.Matches(str, @"\b[0-9]{2}.[0-9]{2}.[0-9]{4}\b"))
+        foreach (Match item in Regex.Matches(str, @"\b\d{2}.\d{2}.\d{4}\b"))
             if (DateTime.TryParseExact(item.Value, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
                 Console.WriteLine(date.ToString(CultureInfo.GetCultureInfo("en-CA").DateTimeFormat.ShortDatePattern));
     }
