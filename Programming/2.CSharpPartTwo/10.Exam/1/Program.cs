39457391de59f4d61ff16d5bbbf8fd850389b693
Jason Zhekov <jasssonpet@gmail.com>
2013-02-04 00:17:20 +0200

    Small fixes

---
+++ Programming/2.CSharpPartTwo/10.Exam/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        Console.WriteLine(Console.ReadLine());
+    }
+}
---
+++ Programming/2.CSharpPartTwo/10.Exam/4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+    }
+}
