cefbce7fdcd967d94543dc075a091d984526b6e5
Jason Zhekov <jasssonpet@gmail.com>
2013-02-07 04:37:15 +0200

    Finish 9.ExamPreparation

---
+++ Programming/2.CSharpPartTwo/9.ExamPreparation/3.Tubes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int N, M;
+
+    static int best;
+    static List<int> tubes = new List<int>();
+
+    static void Input()
+    {
+        N = int.Parse(Console.ReadLine());
+        M = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+            tubes.Add(int.Parse(Console.ReadLine()));
+    }
+
+    static int GetMaxTubes(int length)
+    {
+        int count = 0;
+
+        foreach (int tube in tubes)
+            count += tube / length;
+
+        return count;
+    }
+
+    static void BinarySearch()
+    {
+        int l = 0;
+        int r = (int)2e9;
+
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+
+            if (GetMaxTubes(m) < M) r = m;
+            else l = m + 1;
+        }
+
+        if (GetMaxTubes(l) < M) l--;
+
+        best = l;
+    }
+
+    static void Output()
+    {
+        Console.WriteLine(best);
+    }
+
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt")); // Runs locally only
+#endif
+
+        Input();
+
+        BinarySearch();
+
+        Output();
+    }
+}
---
+++ Programming/2.CSharpPartTwo/9.ExamPreparation/5.AcademyTasks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static List<int> tasks = new List<int>();
+    static int variety;
+
+    static int minTasks;
+
+    static void Input()
+    {
+        foreach (string task in Regex.Split(Console.ReadLine(), ", "))
+            tasks.Add(int.Parse(task));
+
+        variety = int.Parse(Console.ReadLine());
+    }
+
+    static void FindMinTasks()
+    {
+        minTasks = tasks.Count;
+
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            for (int j = i + 1; j < tasks.Count; j++)
+            {
+                if (Math.Abs(tasks[i] - tasks[j]) < variety) continue;
+                
+                int currentTasks = (i + 1) / 2 + (j - i + 1) / 2 + 1;
+
+                if (currentTasks < minTasks) minTasks = currentTasks;
+            }
+        }
+    }
+
+    static void Output()
+    {
+        Console.WriteLine(minTasks);
+    }
+
+    static void Main()
+    {
+#if DEBUG
+    Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        Input();
+
+        FindMinTasks();
+
+        Output();
+    }
+}
