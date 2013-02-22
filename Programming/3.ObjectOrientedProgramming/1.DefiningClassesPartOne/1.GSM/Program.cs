74df81d9cff6515c0bd3cf12d5712a00b12fa512
Jason Zhekov <jasssonpet@gmail.com>
2013-02-22 23:08:42 +0200

    Add OOP

---
+++ Programming/3.ObjectOrientedProgramming/1.DefiningClassesPartOne/1.GSM/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Display
+{
+    public readonly int Width, Height;
+    public readonly int NumberOfColors;
+}
+
+class Battery
+{
+    public readonly string Model;
+    public readonly int HoursIdle, HoursTalk;
+}
+
+class GSM
+{
+    public readonly string Model, Manufacturer;
+    public readonly string Owner;
+    public readonly decimal Price;
+
+    public readonly Display Display = new Display();
+    public readonly Battery Battery = new Battery();
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(new GSM().Display.Height);
+    }
+}
