62777016fbaa042a8a99b63a3c2a3b1f169986b6
Jason Zhekov <jasssonpet@gmail.com>
2013-02-23 01:51:54 +0200

    Add Defining Classes 5

--- Programming/3.ObjectOrientedProgramming/1.DefiningClassesPartOne/1.GSM/Program.cs
+++ Programming/3.ObjectOrientedProgramming/1.DefiningClassesPartOne/1.GSM/Program.cs
@@ -3,16 +3,65 @@ using System.Collections.Generic;
 
 class Display
 {
-    public readonly int Width, Height;
-    public readonly int? NumberOfColors;
+    // Private Constants
+    const uint MaxWidth = 1 << 16;
+    const uint MaxHeight = MaxWidth * 2;
+    const uint MaxNumberOfColors = 1 << 32;
 
-    public Display(int width, int height, int? numberOfColors = null)
+    // Private Fields
+    uint width = 0;
+    uint height = 0;
+    uint? numberOfColors = null;
+
+    // Properties
+    public uint Width
+    {
+        get { return this.width; }
+
+        set
+        {
+            if (value > MaxWidth)
+                throw new ArgumentOutOfRangeException();
+
+            this.width = value;
+        }
+    }
+
+    public uint Height
+    {
+        get { return this.height; }
+
+        set
+        {
+            if (value > MaxHeight)
+                throw new ArgumentOutOfRangeException();
+
+            this.height = value;
+        }
+    }
+
+    public uint? NumberOfColors
+    {
+        get { return this.numberOfColors; }
+
+        set
+        {
+            if (value.GetValueOrDefault() > MaxNumberOfColors)
+                throw new ArgumentOutOfRangeException();
+
+            this.numberOfColors = value;
+        }
+    }
+
+    // Constructors
+    public Display(uint width, uint height, uint? numberOfColors = null)
     {
         this.Width = width;
         this.Height = height;
         this.NumberOfColors = numberOfColors;
     }
 
+    // Methods
     public override string ToString()
     {
         List<string> info = new List<string>();
@@ -29,18 +78,55 @@ class Display
 
 class Battery
 {
-    public readonly Type Model;
-    public readonly int? HoursIdle, HoursTalk;
-
     public enum Type { LiIon, NiMH, NiCd }; // static by default
 
-    public Battery(Type model, int? hoursIdle = null, int? hoursTalk = null)
+    // Private Constants
+    const uint MaxIdleHours = 1000;
+    const uint MaxTalkHours = 500;
+
+    // Private Fields
+    Type model = 0;
+    uint? hoursIdle = null;
+    uint? hoursTalk = null;
+
+    // Properties
+    public Type Model { get; set; }
+
+    public uint? HoursIdle
+    {
+        get { return this.hoursIdle; }
+
+        set
+        {
+            if (value.GetValueOrDefault() > MaxIdleHours)
+                throw new ArgumentOutOfRangeException();
+
+            this.hoursIdle = value;
+        }
+    }
+
+    public uint? HoursTalk
+    {
+        get { return this.hoursTalk; }
+
+        set
+        {
+            if (value.GetValueOrDefault() > MaxTalkHours)
+                throw new ArgumentOutOfRangeException();
+
+            this.hoursIdle = value;
+        }
+    }
+
+    // Constructors
+    public Battery(Type model, uint? hoursIdle = null, uint? hoursTalk = null)
     {
         this.Model = model;
         this.HoursIdle = hoursIdle;
         this.HoursTalk = hoursTalk;
     }
 
+    // Methods
     public override string ToString()
     {
         List<string> info = new List<string>();
@@ -59,12 +145,13 @@ class Battery
 
 class GSM
 {
-    public readonly string Model, Manufacturer;
-    public readonly string Owner;
-    public readonly decimal? Price;
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public string Owner { get; set; }
+    public decimal? Price { get; set; }
 
-    public readonly Display Display;
-    public readonly Battery Battery;
+    public Display Display { get; set; }
+    public Battery Battery { get; set; }
 
     public GSM(string model, string manufacturer, string owner = null,
         decimal? price = null, Display display = null, Battery battery = null)
@@ -105,8 +192,8 @@ class Program
     static void Main()
     {
         Display display = new Display(480, 320);
-        Battery battery = new Battery(Battery.Type.LiIon, hoursTalk: 10);
-
+        Battery battery = new Battery(Battery.Type.LiIon);
+        
         GSM phone = new GSM("iPhone 5", "Apple", display: display, battery: battery);
 
         Console.WriteLine(phone);
