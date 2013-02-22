49f36ee46f5422bcc11f67ec3cd7642d2189cde8
Jason Zhekov <jasssonpet@gmail.com>
2013-02-22 23:40:28 +0200

    Add Defining Classes 2

--- Programming/3.ObjectOrientedProgramming/1.DefiningClassesPartOne/1.GSM/Program.cs
+++ Programming/3.ObjectOrientedProgramming/1.DefiningClassesPartOne/1.GSM/Program.cs
@@ -3,29 +3,61 @@
 class Display
 {
     public readonly int Width, Height;
-    public readonly int NumberOfColors;
+    public readonly int? NumberOfColors;
+
+    public Display(int width, int height, int? numberOfColors = null)
+    {
+        this.Width = width;
+        this.Height = height;
+        this.NumberOfColors = numberOfColors;
+    }
 }
 
 class Battery
 {
     public readonly string Model;
-    public readonly int HoursIdle, HoursTalk;
+    public readonly int? HoursIdle, HoursTalk;
+
+    public Battery(string model, int? hoursIdle = null, int? hoursTalk = null)
+    {
+        this.Model = model;
+        this.HoursIdle = hoursIdle;
+        this.HoursTalk = hoursTalk;
+    }
 }
 
 class GSM
 {
     public readonly string Model, Manufacturer;
     public readonly string Owner;
-    public readonly decimal Price;
+    public readonly decimal? Price;
 
-    public readonly Display Display = new Display();
-    public readonly Battery Battery = new Battery();
+    public readonly Display Display;
+    public readonly Battery Battery;
+
+    public GSM(string model, string manufacturer, string owner = null,
+        decimal? price = null, Display display = null, Battery battery = null)
+    {
+        this.Model = model;
+        this.Manufacturer = manufacturer;
+        this.Owner = owner;
+        this.Price = price;
+        this.Display = display;
+        this.Battery = battery;
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine(new GSM().Display.Height);
+        GSM phone;
+        
+        phone = new GSM("iPhone 4", "Apple", display: new Display(480, 320));
+        Console.WriteLine(phone.Display.Height);
+
+        phone = new GSM("iPhone 5", "Apple", battery: new Battery("Lithium ion"), price: 1000);
+        Console.WriteLine(phone.Battery.Model);
+        Console.WriteLine(phone.Price);
     }
 }
