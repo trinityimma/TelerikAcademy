32f7274f1b2bcbe6cdbc2c5b08cd93e8eb458b81
Jason Zhekov <jasssonpet@gmail.com>
2013-02-26 01:44:43 +0200

    Update

---
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/7.Timer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+static class FuncExtensions
+{
+    public static void SetInterval(this Action f, int t)
+    {
+        while (true)
+        {
+            Thread.Sleep(t * 1000);
+
+            f();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ((Action)(() => Console.WriteLine(DateTime.Now))).SetInterval(1);
+    }
+}
