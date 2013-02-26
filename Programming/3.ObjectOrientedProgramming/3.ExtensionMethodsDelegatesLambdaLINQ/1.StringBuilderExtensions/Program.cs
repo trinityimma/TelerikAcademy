7b147669a73ad426c03dba775d2b6de2b005b999
Jason Zhekov <jasssonpet@gmail.com>
2013-02-26 02:26:01 +0200

    Update

---
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/1.StringBuilderExtensions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+static class StringBuilderExtensions
+{
+    public static StringBuilder SubString(this StringBuilder str, int startIndex, int length)
+    {
+        StringBuilder result = new StringBuilder();
+
+        if (startIndex + length > str.Length)
+            throw new ArgumentOutOfRangeException();
+
+        for (int i = 0; i < length; i++)
+            result.Append(str[startIndex + i]);
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(new StringBuilder("0123456").SubString(4, 3));
+    }
+}
