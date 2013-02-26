33d63db83909338a24f171ea485bb0e764ee755f
Jason Zhekov <jasssonpet@gmail.com>
2013-02-27 01:18:17 +0200

    Update

--- Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/Program.cs
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/Program.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 static class IEnumerableExtensions
 {
     // http://en.wikipedia.org/wiki/Fold_(higher-order_function)
-    private static T Reduce<T>(this IEnumerable<T> items, Func<T, dynamic, T> func, dynamic start = null)
+    private static T Reduce<T>(this IEnumerable<T> items, Func<dynamic, T, T> func, dynamic first = null)
     {
         IEnumerator<T> i = items.GetEnumerator();
 
         i.MoveNext();
-        T previous = start ?? i.Current;
+        T previous = first ?? i.Current;
 
         while (i.MoveNext())
             previous = func(i.Current, previous);
@@ -39,7 +39,7 @@ static class IEnumerableExtensions
 
     public static int Count<T>(this IEnumerable<T> items)
     {
-        return Convert.ToInt32(items.Reduce((_, b) => b + 1, start: 1));
+        return Convert.ToInt32(items.Reduce((a, _) => a + 1, 1));
     }
 
     public static double Average<T>(this IEnumerable<T> items)
@@ -52,13 +52,13 @@ class Program
 {
     static void Main()
     {
-        IEnumerable<int> elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        IEnumerable<int> elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-        Console.WriteLine("Max: {0}", elements.Max<int>());
-        Console.WriteLine("Min: {0}", elements.Min<int>());
-        Console.WriteLine("Sum: {0}", elements.Sum<int>());
+        Console.WriteLine("Max: {0}",     elements.Max<int>());
+        Console.WriteLine("Min: {0}",     elements.Min<int>());
+        Console.WriteLine("Sum: {0}",     elements.Sum<int>());
         Console.WriteLine("Product: {0}", elements.Product<int>());
-        Console.WriteLine("Count: {0}", elements.Count<int>());
+        Console.WriteLine("Count: {0}",   elements.Count<int>());
         Console.WriteLine("Average: {0}", elements.Average<int>());
     }
 }
