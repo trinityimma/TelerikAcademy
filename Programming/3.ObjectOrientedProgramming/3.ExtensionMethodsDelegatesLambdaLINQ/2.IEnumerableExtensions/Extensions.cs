f0dc8b1f7663c171b68784fdd9cbdfa1d9b750c6
Jason Zhekov <jasssonpet@gmail.com>
2013-02-26 22:53:47 +0200

    Update

--- Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/Extensions.cs
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/Extensions.cs
@@ -3,17 +3,6 @@ using System.Collections.Generic;
 
 static class IEnumerableExtensions
 {
-    // Count
-    public static int Count<T>(this IEnumerable<T> elements)
-    {
-        int count = 0;
-
-        foreach (T _ in elements)
-            count++;
-
-        return count;
-    }
-
     // Min / Max
     private static T MinMax<T>(this IEnumerable<T> elements, Func<dynamic, T, bool> predicate)
     {
@@ -23,8 +12,7 @@ static class IEnumerableExtensions
             T best = i.Current;
 
             while (i.MoveNext())
-                if (predicate(best, i.Current))
-                    best = i.Current;
+                if (predicate(best, i.Current)) best = i.Current;
 
             return best;
         }
@@ -40,32 +28,34 @@ static class IEnumerableExtensions
         return MinMax(elements, (a, b) => a > b);
     }
 
-    // Sum / Product
-    private static T SumProduct<T>(this IEnumerable<T> elements, Func<dynamic, T, T> function, dynamic start)
+    // Sum / Product / Count
+    private static T Aggregate<T>(this IEnumerable<T> elements, Func<dynamic, dynamic, dynamic> function, dynamic start)
     {
         T accumulator = start;
 
-        foreach (T item in elements)
-            accumulator = function(item, accumulator);
+        foreach (T item in elements) accumulator = function(item, accumulator);
 
         return accumulator;
     }
 
     public static T Sum<T>(this IEnumerable<T> elements)
     {
-        return SumProduct(elements, (a, b) => a + b, 0);
+        return Aggregate(elements, (a, b) => a + b, 0);
     }
 
     public static T Product<T>(this IEnumerable<T> elements)
     {
-        return SumProduct(elements, (a, b) => a * b, 1);
+        return Aggregate(elements, (a, b) => a * b, 1);
+    }
+
+    public static T Count<T>(this IEnumerable<T> elements)
+    {
+        return Aggregate(elements, (a, b) => b + 1, 0);
     }
 
     // Average
     public static double Average<T>(this IEnumerable<T> elements)
     {
-        Func<dynamic, double> convert = Convert.ToDouble;
-
-        return convert(elements.Sum<T>()) / convert(elements.Count<T>()); // TODO: Optimize
+        return Convert.ToDouble(elements.Sum<T>()) / Convert.ToDouble(elements.Count<T>()); // TODO: Optimize
     }
 }
