60ca242d4a3ee15ecc39b30077ffb14258d90b44
Jason Zhekov <jasssonpet@gmail.com>
2013-02-26 23:42:24 +0200

    Update

--- Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/IEnumerableExtensions.cs
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/2.IEnumerableExtensions/IEnumerableExtensions.cs
@@ -4,56 +4,42 @@ using System.Collections.Generic;
 static class IEnumerableExtensions
 {
     // Min / Max
-    private static T MinMax<T>(this IEnumerable<T> items, Func<T, dynamic, bool> pred)
+    private static T ForEach<T>(this IEnumerable<T> items, Func<T, dynamic, T> func, dynamic start = null)
     {
         IEnumerator<T> i = items.GetEnumerator();
 
         i.MoveNext();
-        T best = i.Current;
+        T best = start ?? i.Current;
 
         while (i.MoveNext())
-            if (pred(i.Current, best)) best = i.Current;
+            best = func(i.Current, best);
 
         return best;
     }
 
     public static T Max<T>(this IEnumerable<T> items)
     {
-        return MinMax(items, (a, b) => a > b);
+        return ForEach(items, (a, b) => a > b ? a : b);
     }
 
     public static T Min<T>(this IEnumerable<T> items)
     {
-        return MinMax(items, (a, b) => a < b);
-    }
-
-    // Sum / Product / Count
-    private static T Accumulate<T>(this IEnumerable<T> items, Func<T, dynamic, dynamic> func, dynamic start = null)
-    {
-        IEnumerator<T> i = items.GetEnumerator();
-
-        i.MoveNext();
-        T acc = start ?? i.Current;
-
-        while (i.MoveNext())
-            acc = func(i.Current, acc);
-
-        return acc;
+        return ForEach(items, (a, b) => a < b ? a : b);
     }
 
     public static T Sum<T>(this IEnumerable<T> items)
     {
-        return Accumulate(items, (a, b) => a + b);
+        return ForEach(items, (a, b) => a + b);
     }
 
     public static T Product<T>(this IEnumerable<T> items)
     {
-        return Accumulate(items, (a, b) => a * b);
+        return ForEach(items, (a, b) => a * b);
     }
 
-    public static T Count<T>(this IEnumerable<T> items)
+    public static int Count<T>(this IEnumerable<T> items)
     {
-        return Accumulate(items, (_, b) => b + 1, 1);
+        return Convert.ToInt32(ForEach(items, (_, b) => b + 1, start: 1));
     }
 
     // Average
