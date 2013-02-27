c9fdaf8f15e3646a7d4607055f957527336bb348
Jason Zhekov <jasssonpet@gmail.com>
2013-02-27 16:12:28 +0200

    Update

---
+++ Programming/3.ObjectOrientedProgramming/3.ExtensionMethodsDelegatesLambdaLINQ/6.SelectNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+static class IEnumerableExtensions
+{
+    public static void Print(this IEnumerable<int> list)
+    {
+        foreach (var item in list) Console.WriteLine(item);
+
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IEnumerable<int> numbers = Enumerable.Range(1, 100);
+
+        numbers.Where(n =>
+            n % 21 == 0
+        ).Print();
+
+        (from n in numbers
+            where n % 21 == 0
+            select n
+         ).Print();
+    }
+}
