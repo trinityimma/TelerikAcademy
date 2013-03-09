300ae2629643093d825f7365284ec56ca34c8fa1
Jason Zhekov <jasssonpet@gmail.com>
2013-03-09 03:43:35 -0800

    Update Program.cs

--- Programming/3.ObjectOrientedProgramming/6.CommonTypeSystem/5.BitArray/Program.cs
+++ Programming/3.ObjectOrientedProgramming/6.CommonTypeSystem/5.BitArray/Program.cs
@@ -4,14 +4,19 @@ class Program
 {
     static void Main()
     {
-        BitArray64 used = new BitArray64(10);
+        BitArray used = new BitArray(65);
 
         used[0] = true;
         Console.WriteLine(used);
-        
+
+        Console.WriteLine();
+
         used[0] = false;
         Console.WriteLine(used);
+
+        Console.WriteLine();
         
+        // Make a second long
         used[used.Count - 1] = true;
         Console.WriteLine(used);
     }
