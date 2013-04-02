c78c4934715fbf65baf702f4b47b15803dfde3c0
Jason Zhekov <jasssonpet@gmail.com>
2013-04-02 15:05:10 +0300

    Update

--- WebDesign/3.JavaScriptPartOne/10.Exam/1.MaxSum/source.js
+++ WebDesign/3.JavaScriptPartOne/10.Exam/1.MaxSum/source.js
@@ -4,20 +4,14 @@ var Solve = (function() {
     return function(args) {
         args.shift() // N
 
-        var arr = args.map(function(n) {
-            return +n
+        var currentSum = 0
+          , maxSum = Number.NEGATIVE_INFINITY
+
+        args.forEach(function(el) {
+            currentSum = +el + Math.max(currentSum, 0)
+
+            maxSum = Math.max(maxSum, currentSum)
         })
-
-          , currentSum = arr[0]
-          , maxSum = currentSum
-
-          , i
-
-        for (i = 1; i < arr.length; i++) {
-            currentSum = Math.max(0, currentSum) + arr[i]
-
-            maxSum = Math.max(currentSum, maxSum)
-        }
 
         return maxSum
     }
