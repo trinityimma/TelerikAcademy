41a80534177fb2de383b676a5c5c04370b76c1f4
Jason Zhekov <jasssonpet@gmail.com>
2013-04-28 00:25:44 +0300

    Update

---
+++ Programming/4.HighQualityCode/8.HighQualityClasses/1.Abstraction/IFigure.cs
@@ -0,0 +1,8 @@
+namespace Abstraction
+{
+    public interface IFigure
+    {
+        double Perimeter { get; }
+        double Area { get; }
+    }
+}
---
+++ Programming/4.HighQualityCode/8.HighQualityClasses/2.CohesionAndCoupling/RectangularCuboid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CohesionAndCoupling
+{
+    class RectangularCuboid
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Depth { get; set; }
+
+        public double Volume
+        {
+            get { return Width * Height * Depth; }
+        }
+
+        public double CalcDiagonalXY()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Width, Height);
+        }
+
+        public double CalcDiagonalYZ()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Height, Depth);
+        }
+
+        public double CalcDiagonalZX()
+        {
+            return GeometryUtils.CalcDistance2D(0, 0, Depth, Width);
+        }
+
+        public double CalcDiagonalXYZ()
+        {
+            return GeometryUtils.CalcDistance3D(0, 0, 0, Width, Height, Depth);
+        }
+    }
+}
