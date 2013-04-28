481db98d2bde0cc55e6f1c46da391f657cc6fed8
Jason Zhekov <jasssonpet@gmail.com>
2013-04-28 07:54:01 +0300

    Update

---
+++ Programming/4.HighQualityCode/11.UnitTesting/1.School/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Student : IComparable<Student>
+{
+    private string name = null;
+    public string Name
+    {
+        get { return this.name; }
+        private set
+        {
+            if (value == null)
+                throw new ArgumentNullException("Name");
+
+            this.name = value;
+        }
+    }
+
+    private int id = 0;
+    public int Id
+    {
+        get { return this.id; }
+        private set
+        {
+            if (!(10000 <= value && value <= 99999))
+                throw new ArgumentOutOfRangeException("Id");
+
+            this.id = value;
+        }
+    }
+
+    public Student(string name, int id)
+    {
+        this.Name = name;
+        this.Id = id;
+    }
+
+    public int CompareTo(Student other)
+    {
+        return this.Id.CompareTo(other.Id);
+    }
+}
