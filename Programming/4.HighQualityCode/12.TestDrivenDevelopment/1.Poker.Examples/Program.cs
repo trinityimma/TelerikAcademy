e56276d1205d6899353ecbbf256ba7435849a404
Jason Zhekov <jasssonpet@gmail.com>
2013-05-06 05:42:27 +0300

    Update

---
+++ Programming/4.HighQualityCode/12.TestDrivenDevelopment/1.Poker.Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Poker.Examples
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPokerHandsChecker checker = new PokerHandsChecker();
+
+            Console.WriteLine(checker.IsStraightFlush((Hand)"J♣ 10♣ 9♣ 8♣ 7♣"));
+        }
+    }
+}
