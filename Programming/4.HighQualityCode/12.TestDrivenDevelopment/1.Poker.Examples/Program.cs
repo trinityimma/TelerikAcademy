18b0829f46901459b2350ff5ec5a918487102daa
Jason Zhekov <jasssonpet@gmail.com>
2013-05-12 23:31:42 +0300

    Update

--- Programming/4.HighQualityCode/12.TestDrivenDevelopment/1.Poker.Examples/Program.cs
+++ Programming/4.HighQualityCode/12.TestDrivenDevelopment/1.Poker.Examples/Program.cs
@@ -6,9 +6,11 @@ namespace Poker.Examples
     {
         static void Main()
         {
+            Console.WriteLine(new Card(CardFace.Ace, CardSuit.Clubs));
+
             IPokerHandsChecker checker = new PokerHandsChecker();
 
-            Console.WriteLine(checker.IsStraightFlush((Hand)"J♣ 10♣ 9♣ 8♣ 7♣"));
+            //Console.WriteLine(checker.IsStraightFlush((Hand)"J♣ 10♣ 9♣ 8♣ 7♣"));
         }
     }
 }
--- Programming/4.HighQualityCode/12.TestDrivenDevelopment/1.Poker/Card.cs
+++ Programming/4.HighQualityCode/12.TestDrivenDevelopment/1.Poker/Card.cs
@@ -6,6 +6,7 @@ namespace Poker
 {
     public class Card : ICard, IComparable<Card>
     {
+        private static readonly Dictionary<string, CardFace> faceEnums = faceStrings.ToDictionary(x => x.Value, x => x.Key);
         private static readonly Dictionary<CardFace, string> faceStrings = new Dictionary<CardFace, string>()
         {
             { CardFace.Two,   "2"  },
@@ -22,6 +23,8 @@ namespace Poker
             { CardFace.King,  "K"  },
             { CardFace.Ace,   "A"  },
         };
+
+        private static readonly Dictionary<string, CardSuit> suitEnums = suitStrings.ToDictionary(x => x.Value, x => x.Key);
         private static readonly Dictionary<CardSuit, string> suitStrings = new Dictionary<CardSuit, string>()
         {
             { CardSuit.Clubs,    "♣" },
@@ -39,30 +42,22 @@ namespace Poker
             this.Suit = suit;
         }
 
-        // TODO: Extension
-        private static TKey GetKey<TKey, TValue>(Dictionary<TKey, TValue> dictionary, TValue value)
-        {
-            return dictionary.First(key => key.Value.Equals(value)).Key;
-        }
         public static Card Parse(string s)
         {
             string faceStr = s.Substring(0, s.Length - 1);
-            CardFace face = GetKey(faceStrings, faceStr);
+            CardFace face = faceEnums[faceStr];
 
             string suitStr = s.Substring(s.Length - 1);
-            CardSuit suit = GetKey(suitStrings, suitStr);
+            CardSuit suit = suitEnums[suitStr];
 
-            return new Card(face, suit);
+            Card card = new Card(face, suit);
+
+            return card;
         }
 
         public static implicit operator Card(String s)
         {
             return Card.Parse(s);
-        }
-
-        public int CompareTo(Card other)
-        {
-            return this.Face.CompareTo(other.Face) * 2 + this.Suit.CompareTo(other.Suit);
         }
 
         public override bool Equals(object obj)
