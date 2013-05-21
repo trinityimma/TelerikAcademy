ab8f61dbd351f07a6bf2ac6b8de84195ccce07ab
Jason Zhekov <jasssonpet@gmail.com>
2013-05-21 11:30:59 +0300

    Update

--- Programming/4.HighQualityCode/20.Exam/CalendarSystem/CalendarSystem/Example.cs
+++ Programming/4.HighQualityCode/20.Exam/CalendarSystem/CalendarSystem/Example.cs
@@ -1,26 +1,15 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace CalendarSystem
 {
-    /*
-     * # Problem 5: Performance Bottlenecks
-     * 
-     * Using StringBuilder for buffering, a custom hash function,
-     * removed all ToList() and using only IEnumerable.
-     * At EventManagerFast - line 88 - Allready sorted by date, but I don't know how to use it
-     * It is also slow, because there is a lot of LINQ and regular expressions.
-     * 
-     * Tests 009-016 are valid and work, but take 30+ seconds.
-     */
-
     public class Example
     {
         public static void Main()
         {
-            Console.SetIn(new StreamReader("../../../CalendarSystem.Tests/Tests/test.010.in.txt"));
+            // Console.SetIn(new StreamReader("../../../CalendarSystem.Tests/Tests/test.010.in.txt"));
             // Console.SetIn(new StreamReader("../../input.txt"));
 
             // IEventsManager eventsManager = new EventsManager();
