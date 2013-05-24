1d2a6ed0b82d1bf842bb3605caf7faa380b8574a
Jason Zhekov <jasssonpet@gmail.com>
2013-05-25 00:21:56 +0300

    Update

---
+++ Programming/5.DataStructuresAndAlgorithms/AlgoAcademy/2.GadzhetoNaIna/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        int n = int.Parse(Console.ReadLine());
+
+        var input = Enumerable.Range(0, n).Select(_ => Console.ReadLine().Split()).
+            Select(line => new KeyValuePair<string, string>(line[0], line[1]));
+
+        var sorted = input.OrderBy(kvp => Regex.Matches(kvp.Value, "1").Count);
+
+        Console.WriteLine(sorted.Last().Key);
+    }
+}
