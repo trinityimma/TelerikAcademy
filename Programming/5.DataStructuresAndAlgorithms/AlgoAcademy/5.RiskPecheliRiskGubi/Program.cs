8f8e4b7bd94aef7f52254fa86c809cea8d8dd666
Jason Zhekov <jasssonpet@gmail.com>
2013-05-25 16:31:31 +0300

    Update

---
+++ Programming/5.DataStructuresAndAlgorithms/AlgoAcademy/5.RiskPecheliRiskGubi/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+static class Program
+{
+    const int MaxDigit = 10;
+    const int Digits = 5;
+
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        int combinations = (int)Math.Pow(MaxDigit, Digits);
+        bool[] visited = new bool[combinations];
+        bool[] forbidden = new bool[combinations];
+
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        foreach (int i in Enumerable.Range(0, int.Parse(Console.ReadLine())))
+            forbidden[int.Parse(Console.ReadLine())] = true;
+
+        IEnumerable<int> steps = new int[] { 1, -1 };
+
+        var queue = new Queue<int>();
+        queue.Enqueue(start);
+
+        int level = 0;
+
+        while (queue.Count != 0)
+        {
+            level++;
+
+            var nextQueue = new Queue<int>();
+
+            while (queue.Count != 0)
+            {
+                int currentConfiguration = queue.Dequeue();
+
+                for (int i = 0; i < Digits; i++)
+                {
+                    foreach (int step in steps)
+                    {
+                        int nextConfiguration = GetNextConfiguration(currentConfiguration, i, step);
+
+                        if (nextConfiguration == end)
+                        {
+                            Console.WriteLine(level);
+                            return;
+                        }
+
+                        if (!forbidden[nextConfiguration] && !visited[nextConfiguration])
+                        {
+                            visited[nextConfiguration] = true;
+                            nextQueue.Enqueue(nextConfiguration);
+                        }
+                    }
+                }
+            }
+
+            queue = nextQueue;
+        }
+
+        Console.WriteLine(-1);
+    }
+
+    private static int GetNextConfiguration(int currentConfiguration, int i, int step)
+    {
+        int powerOf10 = (int)Math.Pow(10, i);
+
+        int digit = (currentConfiguration / powerOf10) % 10;
+        int nextDigit = (digit + step + MaxDigit) % MaxDigit;
+
+        int nextConfiguration = currentConfiguration;
+        if (digit != 0) nextConfiguration -= digit * powerOf10;
+        nextConfiguration += nextDigit * powerOf10;
+
+        return nextConfiguration;
+    }
+}
