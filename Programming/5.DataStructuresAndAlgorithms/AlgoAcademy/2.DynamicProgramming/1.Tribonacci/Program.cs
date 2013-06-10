943f8e5f908833107e62f081cb29543a5ce94fde
Jason Zhekov <jasssonpet@gmail.com>
2013-06-11 02:15:12 +0300

    Update

---
+++ Programming/5.DataStructuresAndAlgorithms/AlgoAcademy/2.DynamicProgramming/1.Tribonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        long t1 = input[0];
+        long t2 = input[1];
+        long t3 = input[2];
+
+        int n = input[3];
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            long t4 = t1 + t2 + t3;
+            t1 = t2;
+            t2 = t3;
+            t3 = t4;
+        }
+
+        Console.WriteLine(t1);
+    }
+}
