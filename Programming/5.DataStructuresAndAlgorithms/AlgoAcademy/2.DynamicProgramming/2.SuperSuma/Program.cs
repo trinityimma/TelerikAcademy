d6c2c9b43ce533c27f25e8156e20bc7d6179ae48
Jason Zhekov <jasssonpet@gmail.com>
2013-06-11 03:02:52 +0300

    Update

---
+++ Programming/5.DataStructuresAndAlgorithms/AlgoAcademy/2.DynamicProgramming/2.SuperSuma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+#if DEBUG
+        Console.SetIn(new System.IO.StreamReader("../../input.txt"));
+#endif
+
+        var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        var sum = Enumerable.Range(1, input[1]).ToArray();
+
+        for (int i = 1; i < input[0]; i++)
+            for (int j = 0, s = 0; j < input[1]; j++)
+                sum[j] = s += sum[j];
+
+        Console.WriteLine(sum.Sum());
+    }
+}
